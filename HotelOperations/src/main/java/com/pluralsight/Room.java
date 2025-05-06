7d84046ad5696f058a676fcffaf60bc1b2a8329c
Bilenie Tilahun <bilumekb@gmail.com>
2025-05-06 08:58:23 -0500

    added comment on room class.

--- HotelOperations/src/main/java/com/pluralsight/Room.java
+++ HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -5,18 +5,18 @@ public class Room {
     private double price;
     private boolean isOccupied;
     private boolean isDirty;
-
+//Generate constructor
 public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty) {
     this.numberOfBeds = numberOfBeds;
     this.price = price;
     this.isOccupied = isOccupied;
     this.isDirty = isDirty;
 }
-
+//Derived getter
 public boolean IsAvailable() {
     return !isOccupied && !isDirty;
 }
-
+// Generate getter and setter
 public int getNumberOfBeds() {
 return numberOfBeds;
 }
